dd5ba90bfc55ec8bf892dcfeecf00c86244c6029
Shailabh <shaisri681@gmail.com>
2023-01-02 21:38:57 +0530

    App Start

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("../src/route/route");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://ShailabhSrivastava:LtR74yQBXKkSdvyd@cluster0.cxb6bki.mongodb.net/TRAIN",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.all("/*", function (req, res) {
+  res
+    .status(404)
+    .send({
+      status: false,
+      message: "Kindly give correct information in path param ! UNDERSTAND",
+    });
+});
+
+app.listen(3000, function () {
+  console.log("Express app running on port " + 3000);
+});
